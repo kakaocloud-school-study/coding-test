66c3a77154cbe816eaf966dbe09a577058190d23
amanecopse <amanecopse@gmail.com>
2024-01-12 22:00:56 +0900

    perf: 77. Combinations.py 개선

--- lim/week1/77. Combinations.py
+++ lim/week1/77. Combinations.py
@@ -3,18 +3,9 @@ from typing import List
 
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        arr = []
-        answers = []
-        def foo(num, count):
-            if num > n + 1:
-                return
-            if count == k:
-                answers.append(list(arr))
-                return
-            arr.append(num)
-            foo(num + 1, count + 1)
-            arr.pop()
-            foo(num + 1, count)
-        
-        foo(1, 0)
-        return answers
+        if k == 0:
+            yield []
+        else:
+            for num in range(1, n+1):
+                for sub_list in self.combine(n-1, k-1):
+                    yield [num] + sub_list
