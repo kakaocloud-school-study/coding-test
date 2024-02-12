127981755e85225889b919ecbdc34625bbf4fa1c
Chae <oumuamua121@gmail.com>
2024-02-12 23:13:16 +0900

    feat: week5

---
+++ chae/week5/sort/SortColors.java
@@ -0,0 +1,10 @@
+package codingTestStudy.week5.sort;
+
+import java.util.Arrays;
+
+public class SortColors {
+    public void sortColors(int[] nums) {
+        Arrays.sort(nums);
+        System.out.println(nums);
+    }
+}
