0d335328a089b6a0f1d9e2307666d98752b6d99e
Chae <oumuamua121@gmail.com>
2024-03-13 18:26:07 +0900

    문제 풀이 수정

--- chae/week8/HouseRobber_4.java
+++ chae/week8/HouseRobber_4.java
@@ -2,20 +2,19 @@ package codingTestStudy.week8;
 
 public class HouseRobber_4 {
     public int rob(int[] nums) {
-        if (nums.length==1){
-            return nums[0];
-        } else if (nums.length==2){
-            return Math.max(nums[0],nums[1]);
+        int impossible= 0;
+        int possible = 0;
+
+        for(int num: nums) {
+            int curRob =  possible + num;
+            int curNoRob = Math.max(impossible, possible);
+
+            impossible = curRob;
+            possible = curNoRob;
+
         }
-        int[] array = new int[nums.length];
-        int max = nums[0];
-        int previousMax = nums[0];
-        for (int i = 2; i<nums.length; i++){
-             array[i] = Math.max(nums[i]+previousMax, nums[i]);
-             previousMax = Math.max(array[i-1],previousMax);
-             max = Math.max(array[i],max);
-        }
-        return Math.max(max,nums[1]);
+
+        return Math.max(possible, impossible);
     }
 
     public static void main(String[] args) {
